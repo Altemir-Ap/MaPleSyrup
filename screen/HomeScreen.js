48a031e26139bb1b7053a1e1b1d11b5fa0bb3b0a
Altemir Almeida <altemir.git@gmail.com>
2020-10-05 00:02:03 +0100

    modifying git ignore

--- screen/HomeScreen.js
+++ screen/HomeScreen.js
@@ -9,11 +9,11 @@ import {
   Paragraph,
 } from 'react-native-paper';
 import { ScrollView } from 'react-native';
-
+import apiKey from './key.js';
 const HomeScreen = () => {
   let [data, setData] = useState([]);
   fetch(
-    'https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&size=20&sort=date,asc&geoPoint=45.5,23.2&apikey=EUpr9gceb0gVxii8dxP69MHTeLyxYujB',
+    `https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&size=20&sort=date,asc&geoPoint=45.5,23.2&apikey=${apiKey}`,
   )
     .then((response) => {
       return response.json();
@@ -24,28 +24,28 @@ const HomeScreen = () => {
     });
   return (
     <ScrollView>
-      
       {data.map((eachData) => (
-
-<Card key = {eachData.id}>
-<Card.Cover key = {eachData.images[0].url} source={{ uri: eachData.images[2].url }} />
-<Card.Content>
-  <Title>{eachData.name}</Title>
-  <Paragraph style={{ fontWeight: 'bold' }}> Segment </Paragraph>
-  <Paragraph> Genre </Paragraph>
-  <Paragraph> Info </Paragraph>
-</Card.Content>
-<Card.Actions>
-  <Button mode="outlined" style={{ marginRight: 10 }} color="black">
-    Address
-  </Button>
-  <Button mode="outlined" color="black">
-    Buy Tickets
-  </Button>
-</Card.Actions>
-</Card>))
-     
-      }
+        <Card key={eachData.id}>
+          <Card.Cover
+            key={eachData.images[0].url}
+            source={{ uri: eachData.images[2].url }}
+          />
+          <Card.Content>
+            <Title>{eachData.name}</Title>
+            <Paragraph style={{ fontWeight: 'bold' }}> Segment </Paragraph>
+            <Paragraph> Genre </Paragraph>
+            <Paragraph> Info </Paragraph>
+          </Card.Content>
+          <Card.Actions>
+            <Button mode="outlined" style={{ marginRight: 10 }} color="black">
+              Address
+            </Button>
+            <Button mode="outlined" color="black">
+              Buy Tickets
+            </Button>
+          </Card.Actions>
+        </Card>
+      ))}
     </ScrollView>
   );
 };
